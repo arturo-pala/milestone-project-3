cafdc07718e014a2fddb59235cb26ba2e2108dfc
arturo-pala <97579462+arturo-pala@users.noreply.github.com>
2022-12-20 12:16:00 -0800

    Update cookbook.py

--- cookbook/cookbook.py
+++ cookbook/cookbook.py
@@ -145,4 +145,24 @@ def all_recipes(page):
     recipes=recipe_pages, recipeCategory=recipeCategory.find(),count_recipes=count_recipes, total_no_of_pages=total_no_of_pages, 
     page=page, page_title='All Recipes at Lemon & Ginger, Recipe Finder', tags=tags)
 
+    @app.route('/browse_recipes/<recipe_category_name>/<page>', methods=['GET'])
+def browse_recipes(recipe_category_name, page):
+    tags = recipes.distinct("recipe_tags")
+    random.shuffle(tags)    
+    #Count the number of recipes in the Database
+    all_recipes = recipes.find({'recipe_category_name': recipe_category_name}).sort([('date_time', pymongo.DESCENDING), 
+    ('_id', pymongo.ASCENDING)]) 
+    count_recipes = all_recipes.count()
+    
+    #Variables for Pagination
+    offset = (int(page) - 1) * 6
+    limit = 6
+    
+    recipe_pages = recipes.find({'recipe_category_name': recipe_category_name}).sort([("date_time", pymongo.DESCENDING), 
+                    ("_id", pymongo.ASCENDING)]).skip(offset).limit(limit)
+    total_no_of_pages = int(math.ceil(count_recipes/limit))
+   
+    return render_template('browse_recipes.html',
+    recipes=recipe_pages, recipeCategory=recipeCategory.find(),count_recipes=count_recipes, total_no_of_pages=total_no_of_pages, 
+    page=page, recipe_category_name=recipe_category_name, page_title='Lemon & Ginger, Recipe Finder', tags=tags)
     
