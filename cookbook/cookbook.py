8e359a75a1fa864b056977ca9a2ae4ccf1611d1a
arturo-pala <97579462+arturo-pala@users.noreply.github.com>
2022-12-20 12:15:01 -0800

    Update cookbook.py

--- cookbook/cookbook.py
+++ cookbook/cookbook.py
@@ -82,3 +82,46 @@ def signin():
         return render_template('index.html', recipes=recipes.find().sort('date_time',pymongo.DESCENDING), 
         recipeCategory=recipeCategory.find(), tags=tags, page=1, page_title='Login at Lemon & Ginger, Recipe Finder', 
         count_featured_recipes=count_featured_recipes)
+
+    @app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username'].lower()
+    password = request.form['password']
+    session['username'] = username
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    user = userDB.find_one({'username' : username})
+    tags = recipes.distinct("recipe_tags")
+    random.shuffle(tags)
+    #Count the number of recipes for the featured slider
+    featured_recipes = recipes.find({'featured_recipe': 'on'})
+    count_featured_recipes = featured_recipes.count() 
+    
+    if not user:
+        session['logged_in'] = False
+        flash('User ' + session['username'] + ' cannot be found on our system. Please try again.')
+        return signin()
+    elif not check_password_hash(user['password'],password):
+        session['logged_in'] = False
+        flash('Incorrect Password, please try again.')
+        return signin()   
+    else:
+        session['logged_in'] = True
+        flash('Welcome ' + user['author_name'].capitalize())
+        return render_template('index.html', recipes=recipes.find().sort('date_time',pymongo.DESCENDING), 
+        recipeCategory=recipeCategory.find(), author=user['author_name'], tags=tags, page=1, 
+        page_title='Welcome to Lemon & Ginger, Recipe Finder', count_featured_recipes=count_featured_recipes)        
+
+    
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    tags = recipes.distinct("recipe_tags")
+    random.shuffle(tags)
+    #Count the number of recipes for the featured slider
+    featured_recipes = recipes.find({'featured_recipe: 'on'})
+    count_featured_recipes = featured_recipes.count()     
+    return render_template('index.html', recipes=recipes.find().sort('date_time',pymongo.DESCENDING), 
+    recipeCategory=recipeCategory.find(),tags = tags, page=1, page_title='Logout of Lemon & Ginger, Recipe Finder',
+    count_featured_recipes=count_featured_recipes)  
+    
